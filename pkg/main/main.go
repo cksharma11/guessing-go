37c72c0b5cc509163fd8fec38d30e0fad62f74ec
Chandan Kumar <cksharma122@gmail.com>
2020-06-08 00:34:25 +0530

    Extract router file

--- pkg/main/main.go
+++ pkg/main/main.go
@@ -5,29 +5,13 @@ import (
 	"log"
 	"net/http"
 	"time"
-
-	h "github.com/cksharma11/guessing/pkg/handler"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 	redisClient := dh.GetDBHandler(GetRedisClient())
-	handlers := h.NewHandlerContext(&redisClient)
-
-	router := mux.NewRouter().StrictSlash(true)
-
-	router.HandleFunc("/signup/{username}", handlers.SignUp).Methods("POST")
-	router.HandleFunc("/guess/{guess}", handlers.WrapAuth(handlers.Guess)).Methods("POST")
-	router.HandleFunc("/current-level", handlers.WrapAuth(handlers.CurrentLevel)).Methods("GET")
-
-	adminRoutes := router.PathPrefix("/admin").Subrouter()
-	adminRoutes.Use(h.WrapAdminAuth)
-	adminRoutes.HandleFunc("/increment-level", handlers.IncrementLevel).Methods("POST")
-	adminRoutes.HandleFunc("/current-level", handlers.CurrentLevel).Methods("GET")
 
 	server := &http.Server{
-		Handler:      router,
+		Handler:      router(&redisClient),
 		Addr:         "127.0.0.1:8080",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
---
+++ pkg/main/router.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	dh "github.com/cksharma11/guessing/pkg/db_handler"
+	h "github.com/cksharma11/guessing/pkg/handler"
+	"github.com/gorilla/mux"
+)
+
+func router(dbClient *dh.DBHandler) *mux.Router {
+	handlers := h.NewHandlerContext(dbClient)
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/signup/{username}", handlers.SignUp).Methods("POST")
+	router.HandleFunc("/guess/{guess}", handlers.WrapAuth(handlers.Guess)).Methods("POST")
+	router.HandleFunc("/current-level", handlers.WrapAuth(handlers.CurrentLevel)).Methods("GET")
+
+	adminRoutes := router.PathPrefix("/admin").Subrouter()
+	adminRoutes.Use(h.WrapAdminAuth)
+	adminRoutes.HandleFunc("/increment-level", handlers.IncrementLevel).Methods("POST")
+	adminRoutes.HandleFunc("/current-level", handlers.CurrentLevel).Methods("GET")
+
+	return router
+}
